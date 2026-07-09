867c761e466819d266f15e948be2e4529e5bc6fe
Pranjal <pranjalsoni1290@gmail.com>
2026-07-09 17:57:13 +0530

    fix: use H2 in-memory DB for context load test

--- src/test/java/CodeArena/CodearenaApplicationTests.java
+++ src/test/java/CodeArena/CodearenaApplicationTests.java
@@ -2,12 +2,20 @@ package CodeArena;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class CodearenaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
